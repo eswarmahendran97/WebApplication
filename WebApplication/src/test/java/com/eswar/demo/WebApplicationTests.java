af78ae2ee0bb8f621834bdf3e2db1f57a5bab6d7
eswarmahendran97 <eswarmahendran97@gmail.com>
2022-01-24 00:33:54 +0530

    Demo

---
+++ WebApplication/src/test/java/com/eswar/demo/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eswar.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
